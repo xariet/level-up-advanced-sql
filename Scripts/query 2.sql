9c0ecce55e9da4e5aef16c3f83c4f96a6feaffa1
xariet <58727818+xariet@users.noreply.github.com>
2023-10-20 00:34:21 +0000

    1- queries

---
+++ Scripts/query 2.sql
@@ -0,0 +1,6 @@
+// Get a list of Sales people with zero sales
+SELECT e.employeeId, e.firstName, e.lastName, COALESCE(SUM(s.salesAmount), 0) AS Total
+FROM employee e
+LEFT JOIN sales s ON e.employeeId = s.employeeId
+WHERE e.title = 'Sales Person' AND s.salesAmount IS NULL
+GROUP BY e.employeeId, e.firstName, e.lastName;
---
+++ Scripts/query 3.sql
@@ -0,0 +1,22 @@
+//get a list of all sales and all customers even if some of the data 
+//has been removed (note that SQLLite doesn't support FULL JOIN)
+-- Left Join to find rows in "sales" with no matching customer
+SELECT s.*, c.*
+FROM sales s
+LEFT JOIN customer c ON s.customerid = c.customerid
+WHERE c.customerid IS NULL
+
+UNION
+
+-- Right Join to find rows in "customer" with no matching sale
+SELECT s.*, c.*
+FROM sales s
+RIGHT JOIN customer c ON s.customerid = c.customerid
+WHERE s.salesId IS NULL
+
+UNION
+
+-- Inner Join to find rows with matching sales and customers
+SELECT s.*, c.*
+FROM sales s
+INNER JOIN customer c ON s.customerid = c.customerid;
