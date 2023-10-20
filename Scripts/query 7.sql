98a32845de9978b4e2405c721564d92f99642656
xariet <58727818+xariet@users.noreply.github.com>
2023-10-20 17:19:56 +1100

    Update query 7.sql

--- Scripts/query 7.sql
+++ Scripts/query 7.sql
@@ -1,4 +1,4 @@
-//create a report showing total sales per each year
+--create a report showing total sales per each year
 with cte as (SELECT salesAmount, strftime('%Y', soldDate) AS year
 FROM sales)
 select year, format("$%.2f", sum(salesAmount)) from Cte 
